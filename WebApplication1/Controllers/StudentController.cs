b7837778648c3918d4297503f084ce0357dbcac2
ZweYaungNiTunGicIntern <zweyaungnitun@gicjp.com>
2024-03-29 16:37:38 +0630

    student portal

---
+++ WebApplication1/Controllers/StudentController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+using WebApplication1.Data;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+
+
+    public class StudentController : Controller
+    {
+
+        public ApplicationDBContext dbContext;
+        public StudentController(ApplicationDBContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+        [HttpGet]
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Add(AddStudentViewModel viewModel)
+        {
+            var student = new Models.Entity.Student
+            {
+                Name = viewModel.Name,
+                Email = viewModel.Email,
+                Phone = viewModel.Phone,
+                Subscribed = viewModel.Subscribed
+            };
+            await dbContext.Students.AddAsync(student);
+            await dbContext.SaveChangesAsync();
+
+            return View();
+        }
+    }
+}
---
+++ WebApplication1/Data/ApplicationDBContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebApplication1.Models.Entity;
+namespace WebApplication1.Data
+{
+    public class ApplicationDBContext : DbContext 
+    {
+        public ApplicationDBContext(DbContextOptions<ApplicationDBContext> options) : base(options) { 
+            
+        }
+
+        public DbSet<Student> Students { get; set; }
+        
+    }
+}
